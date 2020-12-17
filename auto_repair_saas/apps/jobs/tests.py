fbdf318d2d2692ac6c9a1442c79095d9540dd88a
wangonya <kwangonya@gmail.com>
2020-12-17 21:51:41 +0300

    update tests

--- auto_repair_saas/apps/jobs/tests.py
+++ auto_repair_saas/apps/jobs/tests.py
@@ -1,1 +1,12 @@
-# Create your tests here.
+from auto_repair_saas.apps.utils.tests import BaseTestCase
+
+
+class JobsTestCase(BaseTestCase):
+    def test_login_required(self):
+        self.client.logout()
+        response = self.client.get('/jobs/')
+        self.assertRedirects(response, '/auth/login?next=/jobs/')
+
+    def test_get_jobs_page(self):
+        response = self.client.get('/jobs/')
+        self.assertEqual(response.status_code, 200)
