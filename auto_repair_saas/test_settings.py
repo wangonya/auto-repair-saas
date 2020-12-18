02e8ba1e436e78637720135c912cf433c411417e
wangonya <kwangonya@gmail.com>
2020-12-18 07:39:00 +0300

    add db for github workflow

--- auto_repair_saas/test_settings.py
+++ auto_repair_saas/test_settings.py
@@ -3,3 +3,15 @@ from .settings import *
 INSTALLED_APPS.remove('django_tenants')
 INSTALLED_APPS.remove('auto_repair_saas.apps.tenants')
 MIDDLEWARE.remove('auto_repair_saas.apps.tenants.middleware.TenantMiddleware')
+
+if os.environ.get('GITHUB_WORKFLOW'):
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'github_actions',
+            'USER': 'postgres',
+            'PASSWORD': 'postgres',
+            'HOST': '127.0.0.1',
+            'PORT': '5432',
+        }
+    }
