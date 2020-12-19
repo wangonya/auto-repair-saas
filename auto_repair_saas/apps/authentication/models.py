cf00014c82477a35b3b1ee678fde7c2e36f0b759
wangonya <kelvin@jenga-agency.com>
2020-12-19 12:10:35 +0300

    fix tenant creation

--- auto_repair_saas/apps/authentication/models.py
+++ auto_repair_saas/apps/authentication/models.py
@@ -1,17 +1,21 @@
 import random
 import string
 
+from dateutil.relativedelta import relativedelta
 from django.contrib.auth.base_user import BaseUserManager
 from django.contrib.auth.models import AbstractUser
 from django.core.exceptions import ValidationError
 from django.db import models
+from django.utils.datetime_safe import date
+
+from auto_repair_saas.apps.tenants.create_tenant import create_tenant
 
 
 class UserManager(BaseUserManager):
     @staticmethod
     def generate_tenant_schema_name():
         letters = string.ascii_lowercase
-        schema_name = ''.join(random.choice(letters) for i in range(20))
+        schema_name = ''.join(random.choice(letters) for _ in range(20))
         return schema_name
 
     def create_user(self, username, email, password, **extra_fields):
@@ -26,6 +30,9 @@ class UserManager(BaseUserManager):
         )
         user.set_password(password)
         user.save()
+        create_tenant(schema_name=user.schema,
+                      paid_until=date.today() + relativedelta(months=+1),
+                      on_trial=True)
         return user
 
 
--- auto_repair_saas/apps/tenants/create_tenant.py
+++ auto_repair_saas/apps/tenants/create_tenant.py
@@ -1,28 +1,11 @@
 #!/usr/bin/env python
 
-# from celery import shared_task
-from dateutil.relativedelta import relativedelta
 from django.db import DatabaseError
-from django.db.models.signals import post_save
 from django.db.utils import IntegrityError
-from django.dispatch import receiver
-from django.utils.datetime_safe import date
 
 from .models import Tenant, Domain
-from ..authentication.models import User
 
 
-@receiver(post_save, sender=User)
-def create_tenant_signal(sender, instance, created, **kwargs):
-    if created:  # only run this when new users are created
-        # set paid until date one month from now for trial period
-        paid_until = date.today() + relativedelta(months=+1)
-        create_tenant(schema_name=instance.schema,
-                      paid_until=paid_until,
-                      on_trial=True)
-
-
-# @shared_task
 def create_tenant(schema_name='public',
                   paid_until=None,
                   on_trial=False):
@@ -36,6 +19,7 @@ def create_tenant(schema_name='public',
         domain.tenant = tenant
         domain.is_primary = True
         domain.save()
+        print('tenant created')
     except IntegrityError as e:
         if '(domain_url)=(public) already exists.' in e.args[0]:
             # public schema already exists
