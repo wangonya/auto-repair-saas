946aaf728a9b0c5c855f040353b9063c5e9ccb5b
wangonya <kwangonya@gmail.com>
2020-12-23 13:23:12 +0300

    implement update and delete for contacts

--- auto_repair_saas/apps/contacts/tests.py
+++ auto_repair_saas/apps/contacts/tests.py
@@ -1,4 +1,5 @@
 import factory
+from django.contrib.messages import get_messages
 from django.urls import reverse
 from faker import Faker
 
@@ -21,19 +22,40 @@ class ContactsTestCase(BaseTestCase):
         response = self.client.get(reverse('contacts'))
         self.assertEqual(response.status_code, 200)
 
-    def test_get_new_contact_page(self):
-        response = self.client.get(reverse('new-contact'))
-        self.assertEqual(response.status_code, 200)
-
     def test_post_new_contact(self):
         data = {
             'name': 'customer',
             'contact_type': 'client'
         }
-        response = self.client.post(reverse('new-contact'), data)
-        self.assertRedirects(response, reverse('contacts'))
+        response = self.client.post(reverse('contacts'), data)
+        messages = [m.message for m in get_messages(response.wsgi_request)]
+        self.assertTrue(len(messages) == 1)
+        self.assertEqual('Contact created.', messages[0])
 
     def test_post_new_contact_error(self):
-        response = self.client.post(reverse('new-contact'), {})
-        self.assertEqual(response.context['error'],
-                         'Form is invalid.')
+        response = self.client.post(reverse('contacts'), {})
+        messages = [m.message for m in get_messages(response.wsgi_request)]
+        self.assertTrue(len(messages) == 1)
+        self.assertEqual('Form is invalid.', messages[0])
+
+    def test_update_contact(self):
+        contact = ContactFactory()
+        data = {
+            'name': fake.name(),
+            'contact_type': 'client'
+        }
+        response = self.client.post(
+            reverse('update-contact', kwargs={'pk': contact.id}), data
+        )
+        messages = [m.message for m in get_messages(response.wsgi_request)]
+        self.assertTrue(len(messages) == 1)
+        self.assertEqual('Contact updated.', messages[0])
+
+    def test_delete_contact(self):
+        contact = ContactFactory()
+        response = self.client.post(
+            reverse('delete-contact', kwargs={'pk': contact.id})
+        )
+        messages = [m.message for m in get_messages(response.wsgi_request)]
+        self.assertTrue(len(messages) == 1)
+        self.assertEqual('Contact deleted.', messages[0])
--- auto_repair_saas/apps/contacts/views.py
+++ auto_repair_saas/apps/contacts/views.py
@@ -1,9 +1,11 @@
-from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.messages.views import SuccessMessageMixin
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
 from django.views import View
+from django.views.generic import UpdateView, DeleteView
 
 from auto_repair_saas.apps.contacts.forms import NewContactForm
 from auto_repair_saas.apps.contacts.models import Contact
@@ -11,40 +13,46 @@ from auto_repair_saas.apps.contacts.models import Contact
 
 class ContactsView(LoginRequiredMixin, View):
     template_name = 'contacts/index.html'
+    form_class = NewContactForm
 
     def get(self, request):
+        form = self.form_class()
         context = {
             'clients': Contact.objects.filter(contact_type='client'),
-            'suppliers': Contact.objects.filter(contact_type='supplier')
+            'suppliers': Contact.objects.filter(contact_type='supplier'),
+            'form': form
         }
         return render(request, self.template_name, context)
-
-
-class NewContactView(LoginRequiredMixin, View):
-    form_class = NewContactForm
-    template_name = 'contacts/new.html'
-
-    def get(self, request, *args, **kwargs):
-        form = self.form_class()
-        return render(request, self.template_name, {'form': form})
 
     def post(self, request, *args, **kwargs):
         form = self.form_class(request.POST)
         if form.is_valid():
             try:
                 Contact.objects.create(**form.cleaned_data)
+                messages.success(request, 'Contact created.')
                 return HttpResponseRedirect(reverse('contacts'))
             except Exception as e:
-                error = str(e)
-                return render(
-                    request, self.template_name, {
-                        'form': form, 'error': error
-                    }
-                )
+                messages.error(request, str(e))
+                return HttpResponseRedirect(reverse('contacts'))
         else:
             error = 'Form is invalid.'
-            return render(
-                request, self.template_name, {
-                    'form': form, 'error': error
-                }
-            )
+            messages.error(request, error)
+            return HttpResponseRedirect(reverse('contacts'))
+
+
+class UpdateContactView(SuccessMessageMixin, UpdateView):
+    model = Contact
+    form = NewContactForm()
+    fields = [*form.fields]
+    success_url = reverse_lazy('contacts')
+    success_message = 'Contact updated.'
+
+
+class DeleteContactView(SuccessMessageMixin, DeleteView):
+    model = Contact
+    success_url = reverse_lazy('contacts')
+    success_message = 'Contact deleted.'
+
+    def delete(self, request, *args, **kwargs):
+        messages.success(self.request, self.success_message)
+        return super(DeleteContactView, self).delete(request, *args, **kwargs)
